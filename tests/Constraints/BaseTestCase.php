d2965c8acae340334560545f2d03260f6a9bd8ba
kaizer <kaizer@kai-zer.ru>
2017-09-15 11:52:08 +0800

    first commit

---
+++ tests/Constraints/BaseTestCase.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+ * This file is part of the JsonSchema package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JsonSchema\Tests\Constraints;
+
+use JsonSchema\Constraints\Constraint;
+use JsonSchema\Constraints\Factory;
+use JsonSchema\SchemaStorage;
+use JsonSchema\Uri\UriResolver;
+use JsonSchema\Validator;
+
+/**
+ * @package JsonSchema\Tests\Constraints
+ */
+abstract class BaseTestCase extends VeryBaseTestCase
+{
+    /**
+     * @dataProvider getInvalidTests
+     */
+    public function testInvalidCases($input, $schema, $checkMode = Constraint::CHECK_MODE_NORMAL, $errors = array())
+    {
+        $checkMode = $checkMode === null ? Constraint::CHECK_MODE_NORMAL : $checkMode;
+
+        $schemaStorage = new SchemaStorage($this->getUriRetrieverMock(json_decode($schema)));
+        $schema = $schemaStorage->getSchema('http://www.my-domain.com/schema.json');
+
+        $validator = new Validator(new Factory($schemaStorage, null, $checkMode));
+        $checkValue = json_decode($input);
+        $validator->validate($checkValue, $schema);
+
+        if (array() !== $errors) {
+            $this->assertEquals($errors, $validator->getErrors(), print_r($validator->getErrors(), true));
+        }
+        $this->assertFalse($validator->isValid(), print_r($validator->getErrors(), true));
+    }
+
+    /**
+     * @dataProvider getInvalidForAssocTests
+     */
+    public function testInvalidCasesUsingAssoc($input, $schema, $checkMode = Constraint::CHECK_MODE_TYPE_CAST, $errors = array())
+    {
+        $checkMode = $checkMode === null ? Constraint::CHECK_MODE_TYPE_CAST : $checkMode;
+        if ($checkMode !== Constraint::CHECK_MODE_TYPE_CAST) {
+            $this->markTestSkipped('Test indicates that it is not for "CHECK_MODE_TYPE_CAST"');
+        }
+
+        $schemaStorage = new SchemaStorage($this->getUriRetrieverMock(json_decode($schema)));
+        $schema = $schemaStorage->getSchema('http://www.my-domain.com/schema.json');
+
+        $validator = new Validator(new Factory($schemaStorage, null, $checkMode));
+        $checkValue = json_decode($input, true);
+        $validator->validate($checkValue, $schema);
+
+        if (array() !== $errors) {
+            $this->assertEquals($errors, $validator->getErrors(), print_r($validator->getErrors(), true));
+        }
+        $this->assertFalse($validator->isValid(), print_r($validator->getErrors(), true));
+    }
+
+    /**
+     * @dataProvider getValidTests
+     */
+    public function testValidCases($input, $schema, $checkMode = Constraint::CHECK_MODE_NORMAL)
+    {
+        $schemaStorage = new SchemaStorage($this->getUriRetrieverMock(json_decode($schema)));
+        $schema = $schemaStorage->getSchema('http://www.my-domain.com/schema.json');
+
+        $validator = new Validator(new Factory($schemaStorage, null, $checkMode));
+        $checkValue = json_decode($input);
+        $validator->validate($checkValue, $schema);
+
+        $this->assertTrue($validator->isValid(), print_r($validator->getErrors(), true));
+    }
+
+    /**
+     * @dataProvider getValidForAssocTests
+     */
+    public function testValidCasesUsingAssoc($input, $schema, $checkMode = Constraint::CHECK_MODE_TYPE_CAST)
+    {
+        if ($checkMode !== Constraint::CHECK_MODE_TYPE_CAST) {
+            $this->markTestSkipped('Test indicates that it is not for "CHECK_MODE_TYPE_CAST"');
+        }
+
+        $schema = json_decode($schema);
+        $schemaStorage = new SchemaStorage($this->getUriRetrieverMock($schema), new UriResolver());
+        $schema = $schemaStorage->getSchema('http://www.my-domain.com/schema.json');
+
+        $value = json_decode($input, true);
+        $validator = new Validator(new Factory($schemaStorage, null, $checkMode));
+
+        $validator->validate($value, $schema);
+        $this->assertTrue($validator->isValid(), print_r($validator->getErrors(), true));
+    }
+
+    /**
+     * @return array[]
+     */
+    abstract public function getValidTests();
+
+    /**
+     * @return array[]
+     */
+    public function getValidForAssocTests()
+    {
+        return $this->getValidTests();
+    }
+
+    /**
+     * @return array[]
+     */
+    abstract public function getInvalidTests();
+
+    /**
+     * @return array[]
+     */
+    public function getInvalidForAssocTests()
+    {
+        return $this->getInvalidTests();
+    }
+}
---
+++ tests/Constraints/TypeTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the JsonSchema package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JsonSchema\Tests\Constraints;
+
+use JsonSchema\Constraints\TypeCheck\LooseTypeCheck;
+use JsonSchema\Constraints\TypeConstraint;
+
+/**
+ * Class TypeTest
+ *
+ * @package JsonSchema\Tests\Constraints
+ *
+ * @author hakre <https://github.com/hakre>
+ */
+class TypeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @see testIndefiniteArticleForTypeInTypeCheckErrorMessage
+     *
+     * @return array
+     */
+    public function provideIndefiniteArticlesForTypes()
+    {
+        return array(
+            array('integer', 'an integer'),
+            array('number', 'a number'),
+            array('boolean', 'a boolean'),
+            array('object', 'an object'),
+            array('array', 'an array'),
+            array('string', 'a string'),
+            array('null', 'a null', array(), 'array'),
+            array(array('string', 'boolean', 'integer'), 'a string, a boolean or an integer'),
+            array(array('string', 'boolean'), 'a string or a boolean'),
+            array(array('string'), 'a string'),
+        );
+    }
+
+    /**
+     * @dataProvider provideIndefiniteArticlesForTypes
+     */
+    public function testIndefiniteArticleForTypeInTypeCheckErrorMessage($type, $wording, $value = null, $label = 'NULL')
+    {
+        $constraint = new TypeConstraint();
+        $constraint->check($value, (object) array('type' => $type));
+        $this->assertTypeConstraintError(ucwords($label) . " value found, but $wording is required", $constraint);
+    }
+
+    /**
+     * Test uncovered areas of the loose type checker
+     */
+    public function testLooseTypeChecking()
+    {
+        $v = new \StdClass();
+        $v->property = 'dataOne';
+        LooseTypeCheck::propertySet($v, 'property', 'dataTwo');
+        $this->assertEquals('dataTwo', $v->property);
+        $this->assertEquals('dataTwo', LooseTypeCheck::propertyGet($v, 'property'));
+        $this->assertEquals(1, LooseTypeCheck::propertyCount($v));
+    }
+
+    /**
+     * Helper to assert an error message
+     *
+     * @param string         $expected
+     * @param TypeConstraint $actual
+     */
+    private function assertTypeConstraintError($expected, TypeConstraint $actual)
+    {
+        $actualErrors = $actual->getErrors();
+
+        $this->assertCount(1, $actualErrors, 'Failed to assert that Type has exactly one error to assert the error message against.');
+
+        $actualError = $actualErrors[0];
+
+        $this->assertInternalType('array', $actualError, sprintf('Failed to assert that Type error is an array, %s given', gettype($actualError)));
+
+        $messageKey = 'message';
+        $this->assertArrayHasKey(
+            $messageKey, $actualError,
+            sprintf('Failed to assert that Type error has a message key %s.', var_export($messageKey, true))
+        );
+
+        $actualMessage = $actualError[$messageKey];
+
+        $this->assertEquals($expected, $actualMessage); // first equal for the diff
+        $this->assertSame($expected, $actualMessage); // the same for the strictness
+    }
+}
